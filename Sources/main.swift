1b7cffa9039ad737835adccb27cc08478e195aba
Jeff Pharr <jeffpharr@New-iMac.home>
2017-02-22 16:00:59 -0500

    Initial Commit

---
+++ Sources/main.swift
@@ -0,0 +1,118 @@
+//
+//  main.swift
+//  PerfectTemplate
+//
+//  Created by Kyle Jessup on 2015-11-05.
+//	Copyright (C) 2015 PerfectlySoft, Inc.
+//
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Perfect.org open source project
+//
+// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
+// Licensed under Apache License v2.0
+//
+// See http://perfect.org/licensing.html for license information
+//
+//===----------------------------------------------------------------------===//
+//
+
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+// An example request handler.
+// This 'handler' function can be referenced directly in the configuration below.
+func handler(data: [String:Any]) throws -> RequestHandler {
+	return {
+		request, response in
+		// Respond with a simple message.
+		response.setHeader(.contentType, value: "text/html")
+		response.appendBody(string: "<html><title>Hello, world!!</title><body>Hello, world!!!</body></html>")
+		// Ensure that response.completed() is called when your processing is done.
+		response.completed()
+	}
+}
+
+func newHandler(data: [String:Any]) throws -> RequestHandler {
+    return {
+        request, response in
+        // Respond with a simple message.
+        response.setHeader(.contentType, value: "text/html")
+        response.appendBody(string: "<html><title>testing</title><body>Testing 1,2,3</body></html>")
+        // Ensure that response.completed() is called when your processing is done.
+        response.completed()
+    }
+}
+
+func listHandler(data: [String:Any]) throws -> RequestHandler {
+    return {
+        request, response in
+        // Respond with a simple message.
+        response.setHeader(.contentType, value: "application/json")
+        let d: [String:Any] = ["a":1, "b":0.1, "c": true, "d":[2, 4, 5, 7, 8]]
+        
+        do{
+            try response.setBody(json: d)
+        } catch{
+            //...
+        }
+
+        // Ensure that response.completed() is called when your processing is done.
+        response.completed()
+    }
+}
+
+
+// Configuration data for two example servers.
+// This example configuration shows how to launch one or more servers 
+// using a configuration dictionary.
+
+let port1 = 8080, port2 = 8181
+
+let confData = [
+	"servers": [
+		// Configuration data for one server which:
+		//	* Serves the hello world message at <host>:<port>/
+		//	* Serves static files out of the "./webroot"
+		//		directory (which must be located in the current working directory).
+		//	* Performs content compression on outgoing data when appropriate.
+		[
+			"name":"localhost",
+			"port":port1,
+			"routes":[
+				["method":"get", "uri":"/test/", "handler":newHandler],
+				["method":"get", "uri":"/list/", "handler":listHandler],
+				["method":"get", "uri":"/", "handler":handler],
+				["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.staticFiles,
+				 "documentRoot":"./webroot",
+				 "allowResponseFilters":true]
+			],
+			"filters":[
+				[
+				"type":"response",
+				"priority":"high",
+				"name":PerfectHTTPServer.HTTPFilter.contentCompression,
+				]
+			]
+		],
+		// Configuration data for another server which:
+		//	* Redirects all traffic back to the first server.
+		[
+			"name":"localhost",
+			"port":port2,
+			"routes":[
+				["method":"get", "uri":"/**", "handler":PerfectHTTPServer.HTTPHandler.redirect,
+				 "base":"http://localhost:\(port1)"]
+			]
+		]
+	]
+]
+
+do {
+	// Launch the servers based on the configuration data.
+	try HTTPServer.launch(configurationData: confData)
+} catch {
+	fatalError("\(error)") // fatal error launching one of the servers
+}
+
